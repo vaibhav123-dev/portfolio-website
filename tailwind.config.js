0d73c4f06bf226760747facc34d9f89a481784e8
Vaibhav Darvekar <vabdarwekar00@gmail.com>
2022-08-09 21:22:45 +0530

    Folder added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,40 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      primary: 'Playfair Display',
+      body: 'Work Sans',
+    },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        lg: '3rem',
+      },
+    },
     screens: {
-      "2xl": { max: "1535px" },
-      // => @media (max-width: 1535px) { ... }
-
-      xl: { max: "1279px" },
-      // => @media (max-width: 1279px) { ... }
-
-      lg: { max: "1023px" },
-      // => @media (max-width: 1023px) { ... }
-
-      md: { max: "767px" },
-      // => @media (max-width: 767px) { ... }
-
-      sm: { max: "639px" },
-      // => @media (max-width: 639px) { ... }
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    extend: {
+      content: {
+        about: 'url("/src/assets/img/outline-text/about.svg")',
+        portfolio: 'url("/src/assets/img/outline-text/portfolio.svg")',
+        services: 'url("/src/assets/img/outline-text/services.svg")',
+        testimonials: 'url("/src/assets/img/outline-text/testimonials.svg")',
+        contact: 'url("/src/assets/img/outline-text/contact.svg")',
+      },
+      colors: {
+        primary: '#050402',
+        secondary: '#1C1D24',
+        tertiary: '#131419',
+        accent: {
+          DEFAULT: '#ac6b34',
+          hover: '#925a2b',
+        },
+        paragraph: '#878e99',
+      },
     },
   },
   plugins: [],
